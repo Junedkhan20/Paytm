3999157cd5096ed59ccc0a58925028aa4a8f3c3d
Junaid Khan <jkkhan2542@gmail.com>
2026-09-20 22:34:30 +0530

    update gitignore

--- backend/config.js
+++ backend/config.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const MONGODB_URI = process.env.mongodb_uri || 'mongodb+srv://junaidkhanofficial98_db_user:0al1WkJMoZ4br8JF@cluster0.ajcqqa7.mongodb.net/paytmApp';
-const JWT_SECRET = process.env.jwt_secret || 'jksecret';
+const MONGODB_URI = process.env.mongodb_uri || '';
+const JWT_SECRET = process.env.jwt_secret || '';
 
 module.exports = {
     MONGODB_URI,
